5b9fea10d80082ea78522f69c27a7647b2790208
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-05-06 15:08:52 -0400

    added basic math operators and screen updates when user selects buttons

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
 console.log("Welcome to Calculator");
+
+//DOM elements for all calculator buttons and operations
+const screen = document.querySelector('#screen');
+const clearBtn = document.querySelector('.clear');
+const undoBtn = document.querySelector('.undo');
+const divideBtn = document.querySelector('.divide');
+const timesBtn = document.querySelector('.times');
+const minusBtn = document.querySelector('.minus');
+const plusBtn = document.querySelector('.plus');
+const equalsBtn = document.querySelector('.equals');
+const numberBtn = document.querySelectorAll('.num');
+const buttons = document.querySelectorAll('.calc-button');
+
+console.log(clearBtn.textContent)
+
+//Global data storage
+let currentNumber = '';
+let operatorValue = '';
+let display = '';
+let sum = "0";
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, operator, num2) {
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "x") {
+        return multiply(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    } else {
+        return prompt("Error");
+    }
+}
+
+function populateDisplay() {
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const buttonText = button.textContent;
+            console.log(buttonText);
+            
+            if (buttonText == 'C') {
+                display = '';
+                sum = '';
+                screen.textContent = '0';
+            } else {
+                if (screen.textContent === '0') {
+                    screen.textContent = '';
+                }
+                display += buttonText;
+            }
+            screen.textContent = display;
+        })
+    })
+}
+
+populateDisplay();
+
